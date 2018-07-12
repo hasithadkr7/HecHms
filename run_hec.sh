14a1223084c5b6bdb5a28f287d4e029d2a1565ae
hasitha <hasithadkr7@gmail.com>
2018-07-12 05:42:31 +0530

    removing duplicated files

--- run_hec.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-#/home/uwcc-admin/udp_150/hec-hms41/HEC-HMS.sh -s /home/uwcc-admin/udp_150/2008_2_Events/2008_2_Events.script
-/home/uwcc-admin/udp_150/hec-hms41/HEC-HMS.sh -s $1
-echo "hec-hms is running..."
